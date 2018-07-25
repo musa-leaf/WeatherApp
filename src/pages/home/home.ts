2a9fefe0c879e8ad8573824318af50e365de0ef7
musa-leaf <khosamusa42@gmail.com>
2018-07-25 16:18:25 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { OpenWeatherProvider } from '../../providers/open-weather/open-weather';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  weatherData : any ;
+  city : string = "Pretoria";
+  dateObj: number = Date.now();
+  Math : any;
+  
 
+  constructor(public openWeather : OpenWeatherProvider, public navCtrl: NavController, public navParams: NavParams) {
+    this.retrieve();
+    this.Math = Math;
+  }
+
+  doRefresh(refresher){
+    this.retrieve();
+    refresher.complete();
+  }
+
+  cityUpdate(){
+    this.retrieve();
+  }
+
+  retrieve(){
+    this.openWeather.updateWeather(this.city).subscribe(
+      data =>{
+        this.weatherData = data;
+      }
+    );
   }
 
 }
